100b7910021c076e8132486e1ff61744df803cb0
Benjamin Girvan <bengirvan@fb.com>
2023-02-14 16:34:04 +0000

    added card stack to card component

--- src/Home.js
+++ src/Home.js
@@ -3,9 +3,15 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import {CardComponent} from "./components";
 
 function Home() {
+
+    const qSet = [
+        {"q": "Who killed JFK?", "a": true},
+        {"q": "Who is the wrapper m&m?", "a": false},
+        {"q": "How does this effect LeBron's legacy?", "a": true}
+    ]
     return(
         <div>
-       <CardComponent/>
+       <CardComponent qArr={qSet}/>
        </div>
     );
 }

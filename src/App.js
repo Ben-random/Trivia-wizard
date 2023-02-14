6e4f5999c357dd5e7fff339d3d2e8a2c5e60f599
Van Nguyen <vanthoai@fb.com>
2023-02-14 13:52:24 +0000

    fix home route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import React, { useEffect, useState } from 'react';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import {Home} from "./Home";
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./Home";
 
 function App() {
   return (
     <div data-testid="app">
       <BrowserRouter>
-          <Routes path="/">
-            <Home />
+        <Routes>
+          <Route path="/" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </div>

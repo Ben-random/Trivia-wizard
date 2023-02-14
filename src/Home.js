6bf35493f72d34f8b1dd8d925f0f36bd077ed5c3
Benjamin Girvan <bengirvan@fb.com>
2023-02-14 13:38:24 +0000

    Added routes

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState, useParams } from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+
+function Home() {
+    return(
+        <div>
+            <p>Hello, Van - pick a Q</p>
+        </div>
+    );
+}
+
+export {Home};

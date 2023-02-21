6e426cfd60b6f8637f8daac0f157670b9d0deb48
Van Nguyen <vanthoai@fb.com>
2023-02-21 13:02:00 +0000

    added answer buttons and verify correctness

--- src/components/CardComponent/index.js
+++ src/components/CardComponent/index.js
@@ -1,27 +1,53 @@
-import React, {useEffect, useState, useParams, useContext} from "react";
-import {DataContext} from "../../context/DataContext";
-import Card from 'react-bootstrap/Card';
+import React, { useEffect, useState, useParams, useContext } from "react";
+import { DataContext } from "../../context/DataContext";
+import Card from "react-bootstrap/Card";
 import "./style.css";
+import AnswerButton from "../AnswerButton";
+import { decodeHTML } from "../../Utils";
 
 function CardComponent(props) {
-    const { questions } = useContext(DataContext)
-    return<>
-        <div className="body">
-           {questions.length > 0 ? <Card style={{ width: '18rem' }}>
-                <Card.Body>
-                    <Card.Title>Question:</Card.Title>
-                    <Card.Text>
-                    {questions[0].question}
-                    </Card.Text>
-                    <div>
-                    <button type="button">True</button>
-                    <button type="button">False</button>
-                    <button type="button">Next</button>
-                    </div>
-                </Card.Body>
-            </Card> : <></>}
-        </div>
-    </>;
+  const { questions, answers } = useContext(DataContext);
+  const [guessed, setGuessed] = useState(false);
+  const [guess, setGuess] = useState("");
+
+  const handleGuess = (answer) => {
+    setGuessed(true);
+    setGuess(answer);
+  };
+
+  return (
+    <>
+      <div className="body">
+        {questions.length > 0 ? (
+          <Card style={{ width: "18rem" }}>
+            <Card.Body>
+              <Card.Title>Question:</Card.Title>
+              <Card.Text>{decodeHTML(questions[0].question)}</Card.Text>
+              <div>
+                {answers.map((answer, index) => (
+                  <AnswerButton
+                    key={index}
+                    answer={answer}
+                    handleGuess={() => handleGuess(answer)}
+                  />
+                ))}
+                {guessed &&
+                  (guess === questions[0].correct_answer ? (
+                    <h1 className="text-success">Correct!</h1>
+                  ) : (
+                    <h1 className="text-danger">
+                      Incorrect! the answer is {questions[0].correct_answer}
+                    </h1>
+                  ))}
+              </div>
+            </Card.Body>
+          </Card>
+        ) : (
+          <></>
+        )}
+      </div>
+    </>
+  );
 }
 
-export default CardComponent
+export default CardComponent;
